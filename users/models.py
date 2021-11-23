5da3c100321b9773187bb896b7f1d38b3500db77
jinatra <jinatra0816@gmail.com>
2021-11-23 16:17:01 +0900

    모델링 추가: User

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+from core.models import TimeStampModel
+
+
+class User(TimeStampModel):
+    email    = models.EmailField(max_length=200, unique=True)
+    password = models.CharField(max_length=1000)
+    nickname = models.CharField(max_length=50, unique=True)
+
+    class Meta:
+        db_table = 'users'
